5e436beb26f12ee1a5f416a12d8073108f42fc7a
Daniel Rojo <danielrojo@gmail.com>
2021-12-15 18:43:39 +0100

    beers component added with range slider and filter

--- src/components/beers/Beers.vue
+++ src/components/beers/Beers.vue
@@ -1,16 +1,24 @@
 
 <template>
-
     <div class="container pt-5">
-        <Slider v-model="value" />
+        <Slider v-model="range" v-bind:lazy="false" @update="updateRange()" />
         <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-4 my-3" v-for="(beer, index) in beers" :key="index">
+            <div
+                class="col-sm-12 col-md-6 col-lg-4 my-3"
+                v-for="(beer, index) in showBeers"
+                :key="index"
+            >
                 <div class="card h-100">
-                    <img class="card-img-top d-block mx-auto mt-3" v-bind:src="beer.image_url" alt="" style="width: 50px">
+                    <img
+                        class="card-img-top d-block mx-auto mt-3"
+                        v-bind:src="beer.image_url"
+                        alt
+                        style="width: 50px"
+                    />
                     <div class="card-body">
-                        <h4 class="card-title">{{beer.name}}</h4>
-                        <h5>{{beer.abv}}</h5>
-                        <p class="card-text">{{beer.tagline}}</p>
+                        <h4 class="card-title">{{ beer.name }}</h4>
+                        <h5>{{ beer.abv }}</h5>
+                        <p class="card-text">{{ beer.tagline }}</p>
                     </div>
                 </div>
             </div>
@@ -26,7 +34,21 @@ export default {
     data() {
         return {
             beers: [],
-            value: [3,5]
+            range: [3, 5]
+        }
+    },
+    methods: {
+        updateRange() {
+            console.log(this.range);
+
+        }
+
+    },
+    computed: {
+        showBeers() {
+            return this.beers.filter(beer => {
+                return beer.abv >= this.range[0] && beer.abv <= this.range[1];
+            });
         }
     },
     components: { Slider },
@@ -40,6 +62,7 @@ export default {
             .then(data => {
                 console.log(data);
                 this.beers = data;
+
             })
             .catch(error => console.error(error));
     }
